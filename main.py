8e063b8f7e606c8b95b3fe1286d3d109c372d7a7
Gekoga <pdv2005@bk.ru>
2025-11-25 00:21:50 +0300

    Version 1.0 release.

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from random import uniform
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException, NoSuchElementException
+from selenium.common.exceptions import (ElementClickInterceptedException, TimeoutException, NoSuchElementException,
+                                        StaleElementReferenceException)
 from statemachine import StateMachine, State, Event
 from statemachine.transition_list import TransitionList
 from statemachine.exceptions import TransitionNotAllowed
@@ -16,6 +18,7 @@ LOGIN = "Player_1369417"
 PASSWORD = "688025"
 MOB = "Бандит"
 REGEX = r"\p{L}+"
+TIMEOUT = 30
 
 
 def is_error_page() -> bool:
@@ -25,6 +28,22 @@ def is_error_page() -> bool:
 def is_login() -> bool:
     try:
         driver.find_element(By.CLASS_NAME, "login")
+        return True
+    except NoSuchElementException:
+        return False
+
+
+def is_msg_window() -> bool:
+    try:
+        driver.find_element(By.XPATH, "//div[@class='btny button_alt_01']")
+        return True
+    except NoSuchElementException:
+        return False
+
+
+def is_daily_msg_window() -> bool:
+    try:
+        driver.find_element(By.XPATH, "//div[@class='text_msg_quest']")
         return True
     except NoSuchElementException:
         return False
@@ -93,6 +112,23 @@ def login():
         pass
 
 
+def close_msg():
+    try:
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='btny button_alt_01']"))).click()
+    except (TimeoutException, ElementClickInterceptedException):
+        pass
+
+
+def close_daily_msg():
+    try:
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='text_msg_quest']")))
+        driver.execute_script(
+            "hideMsgQuests(); showContent('/quests/quests.php?num=0&amp;pos=0&amp;otvet=3&amp;sluch=0');"
+        )
+    except TimeoutException:
+        pass
+
+
 def go_hunt_from_main_menu():
     try:
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "perg_text")))
@@ -110,7 +146,7 @@ def choose_mob():
             if mob_name == MOB:
                 mobs[i + 1].click()
 
-    except TimeoutException:
+    except (TimeoutException, ElementClickInterceptedException):
         pass
 
 
@@ -132,24 +168,28 @@ def exit_loot():
 
 class MMOBOT(StateMachine, strict_states=True):
     initial = State("Initial", value=0, initial=True, enter=open_page)
-    error_page = State("Error", value=2, enter=reconnect)
-    login_page = State("Login", value=3, enter=login)
-    main_menu = State("Main menu", value=4, enter=go_hunt_from_main_menu)
-    hunt_list = State("Hunt list", value=5, enter=choose_mob)
-    fight_scene = State("Fight", value=6, enter=enable_auto)
-    loot_table = State("Fight", value=7, enter=exit_loot)
+    error_page = State("Error", value=1, enter=reconnect)
+    login_page = State("Login", value=2, enter=login)
+    msg_window = State("Message Window", value=3, enter=close_msg)
+    daily_msg_window = State("Daily Message Window", value=4, enter=close_daily_msg)
+    main_menu = State("Main menu", value=5, enter=go_hunt_from_main_menu)
+    hunt_list = State("Hunt list", value=6, enter=choose_mob)
+    fight_scene = State("Fight Scene", value=7, enter=enable_auto)
+    loot_table = State("Loot Table", value=8, enter=exit_loot)
 
     transitions = TransitionList()
-    transitions.add_transitions(error_page.from_(initial, login_page, main_menu, hunt_list, fight_scene, loot_table, cond=is_error_page))
-    transitions.add_transitions(login_page.from_(initial, error_page, main_menu, hunt_list, fight_scene, loot_table, cond=is_login))
-    transitions.add_transitions(main_menu.from_(initial, error_page, login_page, hunt_list, fight_scene, loot_table, cond=is_main_menu))
-    transitions.add_transitions(hunt_list.from_(initial, error_page, login_page, main_menu, cond=is_hunt_list))
-    transitions.add_transitions(fight_scene.from_(initial, error_page, login_page, hunt_list, cond=is_fight_scene))
-    transitions.add_transitions(loot_table.from_(initial, error_page, login_page, fight_scene, cond=is_loot_table))
+    transitions.add_transitions(error_page.from_(initial, login_page, msg_window, daily_msg_window, main_menu, hunt_list, fight_scene, loot_table, cond=is_error_page))
+    transitions.add_transitions(login_page.from_(initial, error_page, msg_window, daily_msg_window, main_menu, hunt_list, fight_scene, loot_table, cond=is_login))
+    transitions.add_transitions(msg_window.from_(initial, error_page, login_page, daily_msg_window, main_menu, hunt_list, fight_scene, loot_table, cond=is_msg_window))
+    transitions.add_transitions(daily_msg_window.from_(initial, error_page, login_page, msg_window, main_menu, hunt_list, fight_scene, loot_table, cond=is_daily_msg_window))
+    transitions.add_transitions(main_menu.from_(initial, error_page, login_page, msg_window, daily_msg_window, hunt_list, fight_scene, loot_table, cond=is_main_menu))
+    transitions.add_transitions(hunt_list.from_(initial, error_page, login_page, msg_window, daily_msg_window, main_menu, cond=is_hunt_list))
+    transitions.add_transitions(fight_scene.from_(initial, error_page, login_page, msg_window, daily_msg_window, hunt_list, cond=is_fight_scene))
+    transitions.add_transitions(loot_table.from_(initial, error_page, login_page, msg_window, daily_msg_window, fight_scene, cond=is_loot_table))
     loop = Event(transitions, name="Loop")
 
-    def on_enter_state(self, event, state):
-        print(f"Entering '{state.id}' state.")
+    def on_enter_state(self, event: Event, state: State):
+        print(f"Entering '{state.name}' state.")
 
 
 if __name__ == "__main__":
@@ -160,10 +200,15 @@ if __name__ == "__main__":
     wait = WebDriverWait(driver, 10, 1)
 
     bot = MMOBOT()
+    last_state_change = time.time()
     while True:
         try:
             bot.loop()
+            last_state_change = time.time()
         except TransitionNotAllowed:
+            if time.time() - last_state_change > TIMEOUT:
+                open_page()
+
             driver.implicitly_wait(uniform(1.0, 2.0))
 
         driver.implicitly_wait(uniform(0.5, 1.0))
